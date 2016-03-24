3b5ab37ece01322388258b61449e08bf20c79b9d
Anton Evdokimov <free0u@gmail.com>
2016-03-25 00:11:10 +0300

    Call antlr from kotlin

--- src/ru/ifmo/ctd/evdokimov/simple/Main.kt
+++ src/ru/ifmo/ctd/evdokimov/simple/Main.kt
@@ -1,6 +1,11 @@
 package ru.ifmo.ctd.evdokimov.simple
 
+import ru.ifmo.ctd.evdokimov.simple.antlr.ExprLexer
+
 fun main(args: Array<String>) {
     println("Hello")
-
+    val lexer = ExprLexer(null)
+    for (ruleName in lexer.getRuleNames()) {
+        println(ruleName)
+    }
 }
